e4b766fbca042b456509c062f0e1baf990fbdf6e
hepx <hepanxi@163.com>
2014-10-29 17:30:13 +0800

    优化判断AJAX请求

--- jgt-common/src/main/java/org/hepx/jgt/common/ajax/AjaxUtil.java
+++ jgt-common/src/main/java/org/hepx/jgt/common/ajax/AjaxUtil.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * ajax 工具类
+ *
  * @author: Koala
  * @Date: 14-8-21 下午6:32
  * @Version: 1.0
@@ -12,16 +13,19 @@ public class AjaxUtil {
 
     /**
      * 判断是否为AJAX请求
+     *
      * @param webRequest
      * @return
      */
     public static boolean isAjaxRequest(HttpServletRequest webRequest) {
-        String requestedWith = webRequest.getHeader("X-Requested-With");
-        return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+        return (webRequest.getHeader("accept").indexOf("application/json") != -1 || (webRequest
+                .getHeader("X-Requested-With") != null && webRequest.getHeader("X-Requested-With")
+                .indexOf("XMLHttpRequest") != -1));
     }
 
     /**
      * 判断是否为AJAX上传
+     *
      * @param webRequest
      * @return
      */
