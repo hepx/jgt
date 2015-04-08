ade588b06f94b1ede6d64c9209efc7a5579564ff
hepx <hepanxi@163.com>
2015-04-08 11:35:25 +0800

    增加全局异常处理

--- jgt-rbac/src/main/java/org/hepx/rbac/web/controller/IndexController.java
+++ jgt-rbac/src/main/java/org/hepx/rbac/web/controller/IndexController.java
@@ -1,6 +1,7 @@
 package org.hepx.rbac.web.controller;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.hepx.rbac.entity.Resource;
 import org.hepx.rbac.service.ResourceService;
 import org.hepx.rbac.service.UserService;
@@ -20,14 +21,18 @@ public class IndexController {
     private ResourceService resourceService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private SessionDAO sessionDAO;
 
     @RequestMapping("/")
     public String index(Model model) {
-        System.out.println("当用登录用户："+getCurrentUser());
         Set<String> permissions = userService.findPermissions(getCurrentUser());
         List<Resource> menus = resourceService.findMenus(permissions);
+        //左侧菜单
         model.addAttribute("menus", menus);
         SecurityUtils.getSubject().getSession().setAttribute("menus",menus);
+        //当前在线用户
+        model.addAttribute("inline",sessionDAO.getActiveSessions().size());
         return "index";
     }
 
