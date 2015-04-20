db73a619704c8e690dc9b7ff39ad20a5128a9d98
hepx <hepanxi@163.com>
2015-04-20 09:27:03 +0800

    4-18更新

--- jgt-rbac-case/jgt-ticket/src/main/java/org/hepx/ticket/web/controller/TicketController.java
+++ jgt-rbac-case/jgt-ticket/src/main/java/org/hepx/ticket/web/controller/TicketController.java
@@ -12,10 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
@@ -56,6 +54,35 @@ public class TicketController {
         try {
             TicketVo vo = JsonUtil.json2Object(requestData, TicketVo.class);
             tradeService.crateTrade(vo);
+            return ResponseResult.buildSuccessResult().toMap();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseResult.buildFailResult().toMap();
+        }
+    }
+
+    @RequiresPermissions("ticket:update")
+    @RequestMapping(value = "/{id}/update", method = RequestMethod.GET)
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("ticket", ticketService.findOne(id));
+        model.addAttribute("op", "修改");
+        return "ticket/edit";
+    }
+
+    @RequiresPermissions("ticket:update")
+    @RequestMapping(value = "/{id}/update", method = RequestMethod.POST)
+    public String update(Ticket ticket, RedirectAttributes redirectAttributes) {
+        ticketService.updateTicket(ticket);
+        redirectAttributes.addFlashAttribute("msg", "修改成功");
+        return "redirect:/ticket";
+    }
+
+    @RequiresPermissions("ticket:delete")
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    @ResponseBody
+    public Map delete(@PathVariable("id") Long id) {
+        try {
+            ticketService.deleteTicket(id);
             return ResponseResult.buildSuccessResult().toMap();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
