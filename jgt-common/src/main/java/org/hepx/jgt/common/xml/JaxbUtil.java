665166df1c630776d95380bef929824c779418c6
hepx <hepanxi@163.com>
2015-05-15 14:25:54 +0800

    增加jaxb2对xml的转换工具类

---
+++ jgt-common/src/main/java/org/hepx/jgt/common/xml/JaxbUtil.java
@@ -0,0 +1,69 @@
+package org.hepx.jgt.common.xml;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/**
+ * Jaxb转换工具
+ * User: hepx
+ * Date: 2015/5/15
+ * Time: 14:17
+ */
+public class JaxbUtil {
+
+    /**
+     * JavaBean转换成xml
+     * 默认编码UTF-8
+     * @param obj
+     * @return
+     */
+    public static String convertToXml(Object obj) {
+        return convertToXml(obj, "UTF-8");
+    }
+
+    /**
+     * JavaBean转换成xml
+     * @param obj
+     * @param encoding
+     * @return
+     */
+    public static String convertToXml(Object obj, String encoding) {
+        String result = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            //格式化输出
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            //编码方式
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, encoding);
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(obj, writer);
+            result = writer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * xml转换成JavaBean
+     * @param xml
+     * @param c
+     * @return
+     */
+    public static <T> T converyToJavaBean(String xml, Class<T> c) {
+        T t = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(c);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            t = (T) unmarshaller.unmarshal(new StringReader(xml));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+}
